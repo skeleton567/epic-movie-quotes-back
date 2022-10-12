128b2a31596e57cda708f3e85d053ba933c57c4f
skeleton567 <mikkelletoo@gmail.com>
2022-10-12 16:10:06 +0400

    feat: add api athentification routes

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\AuthController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -16,4 +17,12 @@ use Illuminate\Support\Facades\Route;
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
+});
+
+Route::controller(AuthController::class)->group(function () {
+    Route::post('register', 'register')->name('register');
+    Route::post('login', 'login')->name('login');
+    Route::post('logout', 'logout');
+    Route::post('refresh-token', 'refresh');
+    Route::post('authorized-user', 'user');
 });
